f6cf21fe3d114c89a1d65dcf15c74953ee88f001
John Grib <johngrib@woowahan.com>
2017-04-08 12:49:38 +0900

    Controller 기능 적용

--- src/main/java/controller/Controller.java
+++ src/main/java/controller/Controller.java
@@ -2,11 +2,12 @@ package controller;
 
 import model.ResponseMap;
 
+import java.io.DataOutputStream;
 import java.util.Map;
 
 /**
  * 주소에 따라 처리할 로직을 정의하는 Controller Inteface
  */
 public interface Controller {
-    public ResponseMap execute(final Map<String, String> getParams, Map<String, String> postParams);
+    public ResponseMap execute(final Map<String, String> getParams, final Map<String, String> postParams, final DataOutputStream dos);
 }
